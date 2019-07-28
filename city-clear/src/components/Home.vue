a4f961425c8c362d48257ad46f9ac6866b464a80
Chiara <chiara.volonnino@studio.unibo.it>
2019-07-28 17:15:21 +0200

    Refactoring packages.

--- city-clear/src/components/Home.vue
+++ city-clear/src/components/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FindMe/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FindMe from './FindMe.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    FindMe
   }
 }
 </script>
