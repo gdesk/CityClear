f52f97d2465598efb50c99a8b75dc2579d398ff6
Chiara <chiara.volonnino@studio.unibo.it>
2019-07-29 13:37:22 +0200

    Add server implementation.

---
+++ city-clear/server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routing = require('./routers/routing')
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', routing);
+
+
+var port = process.env.PORT || 5051;
+
+app.listen(port, function() {
+    console.log('Express server listening on port: ' + port)
+});
+
+app.use((err, req, res, next) => {
+    if (err.isServer) {
+      console.log(err);
+    }
+    return res.status(err.output.statusCode).json(err.output.payload);
+  });
